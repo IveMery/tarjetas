73ed057c6a9d472528021057cfb617dc0adbd889
IvetteMery <ivette.mery@gmail.com>
2020-09-30 11:48:32 -0300

    modifique contenedor padre

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const divHijo = document.querySelectorAll('.hijo')
 
 
 // Tipo de vista de los contenedores de las tarjetas lista y grid
+
+// const recorrerTarjetas = () => {
+//     for (let hijito of divHijo){
+//         return true
+//     }
+    
+// }
+
+
+
 const contenedor_lista = () => {
-    for (let hijito of divHijo) {
+     for (let hijito of divHijo) {
         hijito.classList.add('ampliarTarjeta')
         hijito.classList.remove('hijo')
     }
 }
 const contenedor_grid = () => {
-    for (let hijito of divHijo) {
+     for (let hijito of divHijo) {
         hijito.classList.remove('ampliarTarjeta')
         hijito.classList.add('hijo')
-    }
+     }
 }
 const vista_lista = () => {
     padreContenedor.classList.add('columna')
